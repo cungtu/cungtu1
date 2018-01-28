fefcb20eb5baafe89da8785418c441a901fa4ec9
cungtu <35164276+cungtu@users.noreply.github.com>
2018-01-28 16:12:26 +0700

    Update c++.cpp

--- c++.cpp
+++ c++.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 using namespace std;
 int main()
-{cout<<"asd";
+{
+	
+	int a,b,s=0 ;
+	cin >>a;
+	cin >>b;
+	
+	for (int i=a;i<=b;i++)
+	{
+		s+=i;
+	}
+cout<<s<<endl;
 return 0;
 }
