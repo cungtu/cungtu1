d9774ed200ba394da0734ba69d9dc5dafc87d302
cungtu <35164276+cungtu@users.noreply.github.com>
2018-05-20 17:43:50 +0700

    Create define.cpp

---
+++ define.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+#include <math.h>
+#define max(a,b) (a>b?a:b)
+#define swap(a,b) (b,a)
+#define cong(a,b) (a+b) 
+#define DEBUG 1
+#define FOR(i,a,b) for(int i=(a),x=(b),i<=x;i++)
+#define FORD(i,b,a) for(int i=(b), _a=(a);i>=_a;i--)
+
+#undef max
+
+int main ()
+{
+	
+	int a=9;
+	int b=6;
+	cout<<max(a,b);
+	
+}
